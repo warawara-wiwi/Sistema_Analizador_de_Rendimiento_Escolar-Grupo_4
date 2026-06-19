51df07a1447caa28080c072f0b4aa02151573ce7
Javier <230262242+warawara-wiwi@users.noreply.github.com>
2026-06-18 21:36:17 -0400

    caca

---
+++ estudiantes = [.py
@@ -0,0 +1,7 @@
+estudiantes = [
+    {"nombre": "Ana", "notas": [6.5, 5.8, 7.0], "asistencia": 90},
+    {"nombre": "Carlos", "notas": [4.0, 3.5, 5.1], "asistencia": 75},
+    {"nombre": "Beatriz", "notas": [5.5, 6.2, 5.8], "asistencia": 88},
+    {"nombre": "Diego", "notas": [3.0, 4.2, 2.8], "asistencia": 60},
+    {"nombre": "Elena", "notas": [6.8, 7.0, 6.9], "asistencia": 95}
+]
