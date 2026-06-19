29cbd842b6a033af987c3c4a5f6e668df5b65fde
unknown <albe.ruiz@duocuc.cl>
2026-06-18 21:49:40 -0400

    Paso 3

--- estudiantes = [.py
+++ estudiantes = [.py
@@ -6,3 +6,16 @@ estudiantes = [
     {"nombre": "Elena", "notas": [6.8, 7.0, 6.9], "asistencia": 95}
 ]
 
+#PASO 3
+def clasificar_estudiantes(lista_con_promedio_final):
+    for estudiante in lista_con_promedio_final:
+        promedio = estudiante["promedio_final"]
+        
+        if promedio >= 5.0:
+            estudiante["estado"] = "Aprobado con Distinción"
+        elif promedio >= 4.0:
+            estudiante["estado"] = "Aprobado"
+        else:
+            estudiante["estado"] = "Reprobado"
+    
+    return lista_con_promedio_final
